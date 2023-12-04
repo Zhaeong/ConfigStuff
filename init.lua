356493296c9471576ad9cb6b49d1749333be8595
Owen Zhang <owen_zzz@hotmail.com>
2023-12-04 02:00:12 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,27 +1,198 @@
 print("Loaded Owen Config")
 
+--[[
+  Installed Plugins:
+SET XDG_CONFIG_HOME=C:\bin
+
+Create folder in 
+XDG_CONFIG_HOME\pack\owenolugins\start
+#owenplugins\opt is optional for lazy loading
+
+and then git clone these repos
+ hrsh7th/cmp-buffer
+ hrsh7th/cmp-cmdline
+ hrsh7th/cmp-nvim-lsp
+ hrsh7th/cmp-path
+ hrsh7th/cmp-vsnip
+ hrsh7th/vim-vsnip
+ hrsh7th/nvim-cmp
+
+ neovim/nvim-lspconfig
+ nvim-treesitter
+ plenary.nvim
+ telescope.nvim
+
+Also need these binaries
+#https://github.com/BurntSushi/ripgrep
+rg.exe
+
+#https://github.com/clangd/clangd/releases/download/17.0.3/clangd-windows-17.0.3.zip
+clangd.exe
+
+]]
 vim.g.mapleader = " "
 
+local set = vim.opt
+
+set.number = true
+
+-- Telescope --------------------------------------------------
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
+vim.keymap.set('n', '<leader>fS', builtin.grep_string, {})
 
---Ctrl-p
+vim.keymap.set('n', '<leader>fs', function()
+    builtin.grep_string({
+        search = vim.fn.input("Grep > ")
+    });
+end)
+
+-- Ctrl-p
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 
+----------------------------------------------------------------
 
 require'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all" (the five listed parsers should always be installed)
-    ensure_installed = { "cpp", "c", "lua" },
+    ensure_installed = {"cpp", "c", "lua"},
 
     highlight = {
-      enable = true,
-  
-      -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-      -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-      -- Using this option may slow down your editor, and you may see some duplicate highlights.
-      -- Instead of true it can also be a list of languages
-      additional_vim_regex_highlighting = false,
+        enable = true,
+
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false
+    }
+}
+
+-- LSP------------------------------------------------------------
+require'lspconfig'.clangd.setup {}
+
+-- Setup language servers.
+local lspconfig = require('lspconfig')
+lspconfig.pyright.setup {}
+lspconfig.tsserver.setup {}
+lspconfig.rust_analyzer.setup {
+    -- Server-specific settings. See `:help lspconfig-setup`
+    settings = {
+        ['rust-analyzer'] = {}
+    }
+}
+
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+    callback = function(ev)
+        -- Enable completion triggered by <c-x><c-o>
+        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+        -- Buffer local mappings.
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        local opts = {
+            buffer = ev.buf
+        }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+        vim.keymap.set('n', '<space>wl', function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, opts)
+        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+        vim.keymap.set({'n', 'v'}, '<space>ca', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+        vim.keymap.set('n', '<space>f', function()
+            vim.lsp.buf.format {
+                async = true
+            }
+        end, opts)
+    end
+})
+
+local cmp = require 'cmp'
+
+cmp.setup({
+    snippet = {
+        -- REQUIRED - you must specify a snippet engine
+        expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+            -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+            -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+        end
     },
-  }
+    window = {
+        -- completion = cmp.config.window.bordered(),
+        -- documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({
+            select = true
+        }) -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({{
+        name = 'nvim_lsp'
+    }, {
+        name = 'vsnip'
+    } -- For vsnip users.
+    -- { name = 'luasnip' }, -- For luasnip users.
+    -- { name = 'ultisnips' }, -- For ultisnips users.
+    -- { name = 'snippy' }, -- For snippy users.
+    }, {{
+        name = 'buffer'
+    }})
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({{
+        name = 'git'
+    } -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+    }, {{
+        name = 'buffer'
+    }})
+})
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({'/', '?'}, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {{
+        name = 'buffer'
+    }}
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({{
+        name = 'path'
+    }}, {{
+        name = 'cmdline'
+    }})
+})
+
+-- Set up lspconfig.
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+require('lspconfig')['clangd'].setup {
+    capabilities = capabilities
+}
