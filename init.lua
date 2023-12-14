328a6228b4ee84bd1f0ba9805a558235d890a955
Owen Zhang <owen_zzz@hotmail.com>
2023-12-14 01:41:55 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -202,5 +202,6 @@ cmp.setup.cmdline(':', {
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 require('lspconfig')['clangd'].setup {
+    cmd = {"clangd", "--header-insertion=never"},
     capabilities = capabilities
 }
